3aa9b93c437ba5ee85668ab16cb733bab22d7bfd
Bryce <br.e.nelson@aggiemail.usu.edu>
2020-03-20 17:03:00 -0600

    Added a package parser class where the main work will be performed

---
+++ src/main/PackageParser.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PackageParser {
+
+    private String[] packagesInput;
+
+    private Map<String, String> packageDependencyMap = new HashMap<String, String>();
+
+    public PackageParser(String[] packagesInput) {
+        this.packagesInput = packagesInput;
+        parsePackagesInput(this.packagesInput);
+    }
+
+    private void parsePackagesInput(String[] packagesInput){
+
+        for(String iterator : packagesInput){
+            String[] splitString = iterator.split(":");
+            this.packageDependencyMap.put(splitString[0], splitString[1]);
+            System.out.println("Package Name: " + splitString[0] + " Dependency: " + splitString[1]);
+        }
+    }
+
+    public Map<String, String> getPackageDependencyMap() {
+        return packageDependencyMap;
+    }
+}
