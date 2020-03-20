5470e2d37592934835624168833f3ad040ccbfa7
Bryce <br.e.nelson@aggiemail.usu.edu>
2020-03-20 17:05:05 -0600

    Built the first test to make sure the PackageParser is building map correctly

--- src/test/PackageCheckerTest.java
+++ src/test/PackageCheckerTest.java
@@ -1,7 +1,19 @@
 package test;
 
+import main.PackageParser;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PackageCheckerTest {
-
+    @Test
+            public void testParsePackages(){
+        String[] packages ={"KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: "};
+        PackageParser packageParser = new PackageParser(packages);
+        Map<String, String> testMap = packageParser.getPackageDependencyMap();
+        assertEquals("Ice", testMap.get("CyberPortal"));
+    }
 }
