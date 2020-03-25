6153882f26051bea652c4ab64bdb89da39c5c178
Bryce <br.e.nelson@aggiemail.usu.edu>
2020-03-25 14:16:16 -0600

    Added a Dependency Checker

---
+++ src/main/DependencyChecker.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.util.Map;
+
+public class DependencyChecker {
+	
+	Map<String, String> allPackages;
+	
+	Map<String, String> packagesWithDependencies;
+	
+	Map<String, String> packagesWithoutDependencies;
+	
+	public void DependencyChecker() {
+		
+	}
+	
+	public void DependencyChecker(Map<String, String> allPackages) {
+		this.allPackages = allPackages;
+	}
+
+	public Map<String, String> getAllPackacges() {
+		return allPackages;
+	}
+
+	public void setAllPackacges(Map<String, String> allPackages) {
+		this.allPackages = allPackages;
+	}
+
+	public Map<String, String> getPackagesWithDependencies() {
+		return packagesWithDependencies;
+	}
+
+	public void setPackagesWithDependencies(Map<String, String> packagesWithDependencies) {
+		this.packagesWithDependencies = packagesWithDependencies;
+	}
+
+	public Map<String, String> getPackagesWithoutDependencies() {
+		return packagesWithoutDependencies;
+	}
+
+	public void setPackagesWithoutDependencies(Map<String, String> packagesWithoutDependencies) {
+		this.packagesWithoutDependencies = packagesWithoutDependencies;
+	}
+	
+	public void sortPackages() {
+		for(Map.Entry<String, String> entry : allPackages.entrySet()) {
+			if("".equalsIgnoreCase(entry.getValue())){
+				packagesWithoutDependencies.put(entry.getKey(), entry.getValue());
+			}
+			else {
+				packagesWithDependencies.put(entry.getKey(), entry.getValue());
+			}
+		}
+	}
+	
+
+}
